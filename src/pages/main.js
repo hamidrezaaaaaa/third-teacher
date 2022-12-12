2cfcc493c4d9086f787b0efb9b90ddbe7c796163
Hamidreza <79729476+hamidrezaaaaaa@users.noreply.github.com>
2022-12-12 14:03:48 +0330

    Create main page.

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,14 +1,59 @@
 import React from "react";
 import styled from "styled-components";
+import logo from "../assets/logo/logo.png";
 
- const Main =()=>{
-    return(
-        <Hooo>third-teacher</Hooo>
-    )
-}
+const Main = () => {
+  return (
+    <Container>
+      <span></span>
+      <SelectMode>
+        <Content>
+          <p className="schools">مدارس</p>
+          <p className="philosophy">فلسفه</p>
+        </Content>
+      </SelectMode>
+    </Container>
+  );
+};
 
 export default Main;
 
-const Hooo =styled.h1`
-color:${props=>props.theme.textColor[2]};
-`
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 5.694vw;
+  span {
+    width: 10.486vw;
+    height: 10.625vw;
+    background-repeat: no-repeat;
+    background-image: url(${logo});
+    background-size: contain;
+    margin: 0 auto;
+  }
+`;
+
+const SelectMode = styled.div`
+  position: relative;
+  border-top: 4px solid #7e7f82;
+`;
+const Content = styled.div`
+  position: absolute;
+  top: 50%;
+  display: flex;
+  gap: 3.472vw;
+  left: 50%;
+  transform: translate(-50%, -59%);
+  background: #ffffff;
+  padding: 0 1.389vw;
+  .schools,
+  .philosophy {
+    margin: 0;
+    font-size: 1.736vw;
+    font-weight: 400;
+    color: ${(props) => props.theme.textColor[1]};
+    cursor: pointer;
+  }
+`;
