debfcbbd5a2e9db03aae336ec5d7b620aeb9eb01
Hamidreza <79729476+hamidrezaaaaaa@users.noreply.github.com>
2022-12-18 13:03:22 +0330

    Create search page for dashboard.

--- src/pages/dashboard/components/search.js
+++ src/pages/dashboard/components/search.js
@@ -1,14 +1,35 @@
 import styled from "styled-components";
+import serachIcon from "../../../assets/icon/search.png";
 
-const Search = ()=>{
-    return(
-        <Container>
-            Search
-        </Container>
-    )
-}
+const Search = () => {
+  return (
+    <Container>
+      <SearchBox>
+        <div className="icon"></div>
+      </SearchBox>
+    </Container>
+  );
+};
 
-const Container =styled.div`
-`
+const Container = styled.div`
+  background: ${(props) => props.theme.background[1]};
+  padding: 2vw;
+  height: 70%;
+`;
+
+const SearchBox = styled.div`
+  width: 90%;
+  margin: auto;
+  background: #ffffff;
+  padding: 2vw;
+  direction: ltr;
+  .icon {
+    width: 4.167vw;
+    height: 4.167vw;
+    background-image: url(${serachIcon});
+    background-size: contain;
+    background-repeat: no-repeat;
+  }
+`;
 
 export default Search;
