0b383745cd93b55899d2ec5a4e52ce9d68960ede
Hamidreza <79729476+hamidrezaaaaaa@users.noreply.github.com>
2022-12-18 12:44:01 +0330

    Add personalinfo page for dashboard.

--- src/pages/dashboard/components/personalInformation.js
+++ src/pages/dashboard/components/personalInformation.js
@@ -1,14 +1,47 @@
 import styled from "styled-components";
 
-const PersonalInformation = ()=>{
-    return(
-        <Container>
-            PersonalInformation
-        </Container>
-    )
-}
+const PersonalInformation = () => {
+  return (
+    <Container>
+      <Form>
+        <input placeholder="نام" />
+        <input placeholder="نام‌ خانوادگی" />
+        <input placeholder="تولد" />
+        <input placeholder="تحصیلات" />
+        <input placeholder="دانشگاه" />
+        <input placeholder="شغل" />
+        <input placeholder="تلفن همراه" />
+        <input placeholder="آدرس ایمیل" />
+        <input placeholder="استان" />
+        <input placeholder="آدرس" />
+        <input placeholder="کدپستی" />
+      </Form>
+    </Container>
+  );
+};
 
-const Container =styled.div`
-`
+const Container = styled.div`
+  background: ${(props) => props.theme.background[1]};
+  padding: 2vw;
+  //   height:100%;
+`;
+const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 0.736vw;
+  width: 80%;
+  margin: auto;
+  input {
+    outline: none;
+    width: 100%;
+    padding: 0.5vw;
+    border: none;
+    background: ${(props) => props.theme.background[4]};
+    font-size: 1.736vw;
+    font-weight: 400;
+    color: ${(props) => props.theme.textColor[1]};
+  }
+`;
 
 export default PersonalInformation;
