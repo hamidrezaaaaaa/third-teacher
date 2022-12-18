5169931aab7c6678d3a23e84f7fbe6490760a483
Hamidreza <79729476+hamidrezaaaaaa@users.noreply.github.com>
2022-12-18 12:54:34 +0330

    Create upload page for dashboard.

--- src/pages/dashboard/components/upload.js
+++ src/pages/dashboard/components/upload.js
@@ -1,14 +1,49 @@
 import styled from "styled-components";
 
-const Upload = ()=>{
-    return(
-        <Container>
-            Upload
-        </Container>
-    )
-}
+const Upload = () => {
+  return (
+    <Container>
+      <Gallery>
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+      </Gallery>
+      <TurnOver>صفحه بعد</TurnOver>
+    </Container>
+  );
+};
 
-const Container =styled.div`
-`
+const Container = styled.div`
+  background: ${(props) => props.theme.background[1]};
+  padding: 2vw;
+`;
+
+const Gallery = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 2vw;
+  width: 90%;
+`;
+
+const Item = styled.div`
+  width: 100%;
+  background: #ffffff;
+  padding: 1.5vw;
+`;
+
+const TurnOver = styled.p`
+  margin: 0;
+  padding: 0;
+  text-align: left;
+  font-size: ${(props) => props.theme.textColor[0]};
+  font-size: 1.5vw;
+  font-weight: 400;
+  margin-top: 5vw;
+`;
 
 export default Upload;
