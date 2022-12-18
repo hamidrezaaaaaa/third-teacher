02f78f427a0570683666c9d403d79c5923b09494
ali salehi <sali.up44@gmail.com>
2022-12-18 17:05:27 +0330

    ali-first-commit

--- src/components/carousel/components/card.js
+++ src/components/carousel/components/card.js
@@ -2,14 +2,15 @@ import React from "react";
 import styled from "styled-components";
 import back from "../../../assets/pic/cover.png";
 
-const Card =({img,title,expand})=>{
+const Card =({img,title,expand,index})=>{
     return(
         <Container>
-          <Image>
+          <Image className="kos">
             <img src={img} alt="philosophy"/>
-            <div className="curtain"></div>
+            {/* <div className="curtain"></div> */}
           </Image>
-          <Title>
+          <Title className="title">
+            {index}
             {title}
           </Title>
         </Container>
@@ -18,25 +19,31 @@ const Card =({img,title,expand})=>{
 
 const Container =styled.div`
 display:flex;
+width:100%;
 flex-direction:column;
+// border:2px solid red;
+height:100%;
+height:300px !important;
 align-items:center;
 justify-content:center;
 background:${props=>props.theme.background[0]};
 padding:0.764vw 0 3.125vw;
-max-width:13.889vw;
-min-width:13.889vw;
-min-height:21.528vw;
+// max-width:13.889vw;
+// min-width:13.889vw;
+// min-height:21.528vw;
 `
 
 const Image =styled.div`
-  // width:100%;
+  width:80%;
   //  height:100%;
   background:${props=>props.theme.background[2]};
   border-radius:100%;
   margin:0 1vw 2.083vw;
   position:relative;
   z-index:10;
+  // border:4px solid white;
   &:before{
+    // border:2px solid white;
     content:'';
     display:block;
     position: absolute;
@@ -45,12 +52,18 @@ const Image =styled.div`
     background-image: url(${back});
     background-size: contain;
     background-repeat: no-repeat;
-    top: -20px;
-    right: 20px;
+    // right: 20px;
+    // top: -20px;
+    left:-11%;
+    top:-11%;
     z-index:-1;
   }
   img{
+    // max-width:120px;
+    // max-height:120px;
+    // height:320px;
     width:100%;
+    // border:4px solid red;
     height:auto;
     object-fit:contain;
     transform: translate(-1px, 13px);
--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -1,39 +1,146 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Card from "./components/card";
 import zar from "../../assets/pic/zartosht.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, Pagination, Navigation } from "swiper";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "./index.scss"
+// import "./styles.css";
 
 const Carousel = () => {
+  let mamad =[1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+  let [main, setMain]= useState(0)
+  let [havali, setHavali] = useState([])
+  // let ghabli=13
+  // let ghabli2=12
+  // let badi =1
+  // let badi2=2
+
+
   return (
     <Container>
-      <Arrow>
+      {/* <Arrow>
         <span></span>
         <span></span>
         <span></span>
-      </Arrow>
+      </Arrow> */}
 
-      <Gallery>
-        <Card img={zar} title="زرتشت" />
-        <Card img={zar} title="زرتشت" />
-        <Card img={zar} title="زرتشت" />
-        <Card img={zar} title="زرتشت" />
-        <Card img={zar} title="زرتشت" />
-        <Card img={zar} title="زرتشت" />
-        <Card img={zar} title="زرتشت" />
-      </Gallery>
 
-      <Arrow>
+      <Swiper
+        dir="rtl"
+      centeredSlides={true}
+      slidesPerView={7}
+      spaceBetween={0}
+      slidesPerGroup={1}
+      loop={true}
+      // pagination={{
+      //   clickable: true,
+      // }}
+      coverflowEffect={{
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      }}
+      navigation={true}
+        modules={[Pagination,EffectCoverflow, Navigation]}
+      className="mySwiper"
+    //  initialSlide={0}
+     onActiveIndexChange={(e)=> {console.log(e.realIndex)
+    //  setMain(e.realIndex)
+    let ghabli
+    let badi
+    let ghabli2
+    let badi2
+    let ghabli3
+    let badi3
+
+    ghabli = e.realIndex - 1
+    badi = e.realIndex + 1
+    badi2 = e.realIndex + 2
+    ghabli2 = e.realIndex -2
+    badi3 = e.realIndex + 3
+    ghabli3 = e.realIndex -3
+    
+    if(badi == 12){
+      badi3 = 0
+    }
+    if(badi == 13){
+      badi2 = 0
+      badi3 = 1
+    }
+    if(badi == 14){
+      badi = 0
+      badi2 = 1
+      badi3 = 2
+    }
+     if(ghabli == -1){
+        ghabli = 13
+        ghabli2= 12
+        ghabli3= 11
+      }
+      if(ghabli == 0){
+        ghabli2= 13
+        ghabli3= 12
+      }
+      if(ghabli == 1){
+        ghabli2= 0
+        ghabli3= 13
+      }
+     setHavali([badi3,badi2, badi , e.realIndex , ghabli, ghabli2,ghabli3])
+
+}}
+    >
+      {
+        
+      }
+    {mamad.map((i,index) =>{
+      console.log(havali)
+      
+return (
+      <SwiperSlide className={havali[0] == index ? "badi3" :havali[1] == index ? "badi2" : havali[2] == index ? 'badi' : havali[3] == index ? "alan" : havali[4] == index ? "ghabli" : havali[5] == index ? "ghabli2" : havali[6] == index ? "ghabli3" : "hich"}
+       >
+          <Card img={zar} index={index} title="زرتشت" />
+          {/* <h1 style={{zIndex:"100",backgroundColor:"red"}}> */}
+
+          {/* {index} */}
+          {/* </h1> */}
+          
+      </SwiperSlide>)
+    })}
+    </Swiper>
+
+      {/* <Gallery>
+        <Card img={zar} title="زرتشت" />
+        <Card img={zar} title="زرتشت" />
+        <Card img={zar} title="زرتشت" />
+        <Card img={zar} title="زرتشت" />
+        <Card img={zar} title="زرتشت" />
+        <Card img={zar} title="زرتشت" />
+        <Card img={zar} title="زرتشت" />
+      </Gallery> */}
+
+      {/* <Arrow>
         <span></span>
         <span></span>
         <span></span>
-      </Arrow>
+      </Arrow> */}
     </Container>
   );
 };
 
 const Container = styled.div`
-  width: 100%;
+  width:70%;
+  margin:auto;
   display: flex;
+  // align-items:flex-end;
+  // padding:0 40px;
+  // border:9px solid red;
   justify-content: center;
   gap: 2.083vw;
   margin: 4.861vw auto;
@@ -42,13 +149,13 @@ const Container = styled.div`
 const Gallery = styled.div`
   width: 70%;
   display: flex;
-  gap: 2.083vw;
+  // gap: 2.083vw;
   overflow-x: auto;
 `;
 
 const Arrow = styled.div`
   display: flex;
-  gap: 0.694vw;
+  // gap: 0.694vw;
   align-items: center;
 
   span {
