a823f5f566af8b537928f512a16b18f32cf4d3c4
Hamidreza <79729476+hamidrezaaaaaa@users.noreply.github.com>
2022-12-21 00:11:05 +0330

    Add htaccess for deploy test on hamidreza domain.

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -1,11 +1,10 @@
-import React, { useRef, useState,useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import styled from "styled-components";
 import Card from "./components/card";
-import zar from "../../assets/pic/zartosht.png";
 import carouselData from "../../data/Philosophers.js";
 import { useNavigate } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay,EffectCoverflow, Pagination, Navigation } from "swiper";
+import { Autoplay, EffectCoverflow, Pagination, Navigation } from "swiper";
 
 // Import Swiper styles
 import "swiper/css";
@@ -14,21 +13,20 @@ import "swiper/css/navigation";
 
 const Carousel = () => {
   const navigate = useNavigate();
-  let [sequence, setSequence] = useState([])
-  let [main,setMain] = useState()
-  let [showModule,setShowModule] = useState(false)
+  let [sequence, setSequence] = useState([]);
+  let [main, setMain] = useState();
+  let [showModule, setShowModule] = useState(false);
   const modal = useRef(null);
   useOutsideAlerter(modal);
 
   function useOutsideAlerter(ref) {
     useEffect(() => {
-
       function handleClickOutside(event) {
         if (ref.current && !ref.current.contains(event.target)) {
-          setShowModule(false)
+          setShowModule(false);
         }
       }
-      
+
       // Bind the event listener
       document.addEventListener("mousedown", handleClickOutside);
       return () => {
@@ -36,170 +34,198 @@ const Carousel = () => {
       };
     }, [ref]);
   }
-  
+
   return (
     <Container>
-      <Arrow className="PrevSlide" >
+      <Arrow className="PrevSlide">
         <span></span>
         <span></span>
         <span></span>
       </Arrow>
 
       <Swiper
-      navigation={{
-        nextEl: ".NextSlide",
-        prevEl: ".PrevSlide"
-      }
-      }
+        navigation={{
+          nextEl: ".NextSlide",
+          prevEl: ".PrevSlide",
+        }}
         dir="rtl"
-      centeredSlides={true}
-      slidesPerView={7}
-      spaceBetween={0}
-      slidesPerGroup={1}
-      loop={true}
-      noSwiping={true}
-      noSwipingSelector="div"
-      // autoplay={{
-      //   delay: 97500,
-      //   disableOnInteraction: false,
-      // }}
-        modules={[Autoplay,Pagination,EffectCoverflow, Navigation]}
-      className="mySwiper"
+        centeredSlides={true}
+        slidesPerView={7}
+        spaceBetween={0}
+        slidesPerGroup={1}
+        loop={true}
+        noSwiping={true}
+        noSwipingSelector="div"
+        // autoplay={{
+        //   delay: 97500,
+        //   disableOnInteraction: false,
+        // }}
+        modules={[Autoplay, Pagination, EffectCoverflow, Navigation]}
+        className="mySwiper"
+        onActiveIndexChange={(e) => {
+          let before1;
+          let next1;
+          let before2;
+          let next2;
+          let before3;
+          let next3;
 
-     onActiveIndexChange={(e)=> {
-    let before1
-    let next1
-    let before2
-    let next2
-    let before3
-    let next3
+          before1 = e.realIndex - 1;
+          next1 = e.realIndex + 1;
+          next2 = e.realIndex + 2;
+          before2 = e.realIndex - 2;
+          next3 = e.realIndex + 3;
+          before3 = e.realIndex - 3;
 
-    before1 = e.realIndex - 1
-    next1 = e.realIndex + 1
-    next2 = e.realIndex + 2
-    before2 = e.realIndex -2
-    next3 = e.realIndex + 3
-    before3 = e.realIndex -3
-    
-    if(next1 == 12){
-      next3 = 0
-    }
-    if(next1 == 13){
-      next2 = 0
-      next3 = 1
-    }
-    if(next1 == 14){
-      next1 = 0
-      next2 = 1
-      next3 = 2
-    }
-     if(before1 == -1){
-        before1 = 13
-        before2= 12
-        before3= 11
-      }
-      if(before1 == 0){
-        before2= 13
-        before3= 12
-      }
-      if(before1 == 1){
-        before2= 0
-        before3= 13
-      }
-     setSequence([next3,next2, next1 , e.realIndex , before1, before2,before3])
+          if (next1 == 12) {
+            next3 = 0;
+          }
+          if (next1 == 13) {
+            next2 = 0;
+            next3 = 1;
+          }
+          if (next1 == 14) {
+            next1 = 0;
+            next2 = 1;
+            next3 = 2;
+          }
+          if (before1 == -1) {
+            before1 = 13;
+            before2 = 12;
+            before3 = 11;
+          }
+          if (before1 == 0) {
+            before2 = 13;
+            before3 = 12;
+          }
+          if (before1 == 1) {
+            before2 = 0;
+            before3 = 13;
+          }
+          setSequence([
+            next3,
+            next2,
+            next1,
+            e.realIndex,
+            before1,
+            before2,
+            before3,
+          ]);
+        }}
+      >
+        {carouselData.map((i, index) => {
+          return (
+            <SwiperSlide
+              key={i}
+              onClick={() => {
+                setShowModule(true);
+                setMain(index);
+              }}
+              className={
+                sequence[0] == index
+                  ? "next3"
+                  : sequence[1] == index
+                  ? "next2"
+                  : sequence[2] == index
+                  ? "next1"
+                  : sequence[3] == index
+                  ? "now"
+                  : sequence[4] == index
+                  ? "before1"
+                  : sequence[5] == index
+                  ? "before2"
+                  : sequence[6] == index
+                  ? "before3"
+                  : "none"
+              }
+            >
+              <Card img={carouselData[index].image} title={carouselData[index].Philosopher} />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
 
-}}
-    >
-      
-    {carouselData.map((i,index) =>{
-      
-return (
-      <SwiperSlide key={i} onClick={() => {setShowModule(true); setMain(index)}} className={sequence[0] == index ? "next3" :sequence[1] == index ? "next2" : sequence[2] == index ? 'next1' : sequence[3] == index ? "now" : sequence[4] == index ? "before1" : sequence[5] == index ? "before2" : sequence[6] == index ? "before3" : "none"}
-       >
-          <Card img={zar} title={carouselData[index].Philosopher} />
-          
-      </SwiperSlide>
-       
-      )
-    })}
-    </Swiper>
-
-    {
-      showModule &&
-      <Module className="tooltip" ref={modal}>
-          <BorderTop/>
+      {showModule && (
+        <Module className="tooltip" ref={modal}>
+          <BorderTop />
           <Philosopher>{carouselData[main].Philosopher}</Philosopher>
           <ShortInfo>{carouselData[main].shortInfo}</ShortInfo>
-        
-        <BottomLinks>
-            <CloseButton onClick={() => {setShowModule(false)}}>بستن</CloseButton>
-            <More onClick={() => {
-              navigate(`${carouselData[main].id + 1}`);
-            }}>ادامه</More>
-        </BottomLinks>
 
-       </Module>
-    }
-<Arrow className="NextSlide" >
+          <BottomLinks>
+            <CloseButton
+              onClick={() => {
+                setShowModule(false);
+              }}
+            >
+              بستن
+            </CloseButton>
+            <More
+              onClick={() => {
+                navigate(`${carouselData[main].id + 1}`);
+              }}
+            >
+              ادامه
+            </More>
+          </BottomLinks>
+        </Module>
+      )}
+      <Arrow className="NextSlide">
         <span></span>
         <span></span>
         <span></span>
       </Arrow>
     </Container>
   );
-
 };
 
-const ShortInfo =styled.span`
-padding:15px 5px; 
-color:#a58463
-`
+const ShortInfo = styled.span`
+  padding: 15px 5px;
+  color: #a58463;
+`;
 
-const Philosopher =styled.span`
-font-size:1.5rem; 
-margin-top:-4.5% ; 
-text-align:center;
-color:#4f594e;
-`
+const Philosopher = styled.span`
+  font-size: 1.5rem;
+  margin-top: -4.5%;
+  text-align: center;
+  color: #4f594e;
+`;
 
-const BorderTop =styled.div`
-margin-top:-23px;
-border-top:2.9px solid #fe9900 ;
-display:flex; width:25%;
-justify-content:space-between
-`
+const BorderTop = styled.div`
+  margin-top: -23px;
+  border-top: 2.9px solid #fe9900;
+  display: flex;
+  width: 25%;
+  justify-content: space-between;
+`;
 
+const BottomLinks = styled.div`
+  display: flex;
+  width: 98%;
+  margin: auto;
+  margin-bottom: -10px;
+  justify-content: space-between;
+`;
 
-const BottomLinks =styled.div`
-display:flex;
-width:98%;
-margin:auto;
-margin-bottom:-10px;
-justify-content:space-between;
-`
+const CloseButton = styled.div``;
+const More = styled.div``;
 
-const CloseButton =styled.div`
-`
-const More =styled.div`
-`
-
-const Module =styled.div`
-position:absolute;
-top:75px;
-display:flex;
-flex-direction:column;
-justify-content:space-between;
-align-items:center;
-height:auto;
-height:50%;
-font-size:1rem;
-width:34.5vw;
-padding:20px 10px;
-background-color:white;
-border:3px solid #4f594e;
-border-radius:7.5px;
-z-index:10;
-`
+const Module = styled.div`
+  position: absolute;
+  top: 75px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: auto;
+  height: 50%;
+  font-size: 1rem;
+  width: 34.5vw;
+  padding: 20px 10px;
+  background-color: white;
+  border: 3px solid #4f594e;
+  border-radius: 7.5px;
+  z-index: 10;
+`;
 
 const Container = styled.div`
 
--- src/components/philosopher/Philosopher.js
+++ src/components/philosopher/Philosopher.js
@@ -2,90 +2,88 @@ import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
 import carouselData from "../../data/Philosophers";
-import cover from "../../assets/pic/cover2.png"
-import zar from "../../assets/pic/zartosht.png"
+import cover from "../../assets/pic/cover2.png";
+
 function Philosopher() {
-  const {id} = useParams()
-return (
-  <Container>
-    <Text>
-      <Title>
-      {carouselData[id-1].Philosopher} {id}
-      </Title>
-      <Data>
-      {carouselData[id-1].longInfo}
-      </Data>
-    </Text>
-    <Image>
-      <img src={carouselData[3].image}></img>
-    </Image>
-  </Container>
-)
+  const { id } = useParams();
+  return (
+    <Container>
+      <Text>
+        <Title>
+          {carouselData[id - 1].Philosopher} {id}
+        </Title>
+        <Data>{carouselData[id - 1].longInfo}</Data>
+      </Text>
+      <Image>
+        <img src={carouselData[id - 1].image}></img>
+      </Image>
+    </Container>
+  );
 }
 
 const Text = styled.div`
   display: flex;
-  width:70%;
+  width: 70%;
   flex-direction: column;
   gap: 2.083vw;
   align-items: flex-start;
-  padding:10px;
-  cursor:pointer;
+  padding: 10px;
+  cursor: pointer;
 `;
 
 const Title = styled.h1`
-  color:#4f594e;
+  color: #4f594e;
   display: flex;
   flex-direction: column;
   align-items: center;
-  cursor:pointer;
+  cursor: pointer;
 `;
 
 const Data = styled.span`
-color:#875c30;
-display: flex;
-flex-direction: column;
-padding-right:4vw;
-gap: 2.083vw;
-align-items: center;
-cursor:pointer;
-`
+  color: #875c30;
+  display: flex;
+  flex-direction: column;
+  padding-right: 4vw;
+  gap: 2.083vw;
+  align-items: center;
+  cursor: pointer;
+`;
 
-const Image =styled.div`
-  width:25%;
-  background:${props=>props.theme.background[2]};
-  border-radius:50%;
+const Image = styled.div`
+  width: 25%;
+  background: ${(props) => props.theme.background[2]};
+  border-radius: 50%;
   margin: 5.083vh 1vw 2.083vw;
-  position:relative;
-  z-index:0;
-  &:before{
-    content:'';
-    display:block;
+  position: relative;
+  z-index: 0;
+  &:before {
+    content: "";
+    display: block;
     position: absolute;
-    padding-top:100%;
+    padding-top: 100%;
     background-image: url(${cover});
     background-size: contain;
     background-repeat: no-repeat;
     right: 20px;
-    left:-11%;
-    top:-11%;
-    z-index:-1;
+    left: -11%;
+    top: -11%;
+    z-index: -1;
   }
-  img{
-    width:100%;
-    object-fit:contain;
+  img {
+    width: 100%;
+    object-fit: contain;
     transform: translate(-1px, 23px);
   }
-`
+`;
 
 const Container = styled.div`
-  display:flex;
-  margin:auto;
-  width:85%;
-  justify-content:space-between;
+  display: flex;
+  margin: auto;
+  width: 85%;
+  justify-content: space-between;
   flex-direction: row;
-  height:100%;
-  cursor:pointer;
+  height: 100%;
+  cursor: pointer;
 `;
 
 export default Philosopher;
