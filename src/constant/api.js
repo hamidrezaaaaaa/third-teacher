ae87cf303005c4d97ea1ea51e4d722edfeb40b60
Hamidreza <79729476+hamidrezaaaaaa@users.noreply.github.com>
2023-04-21 03:40:34 +0330

    Change base url for new server.

--- src/constant/api.js
+++ src/constant/api.js
@@ -1,2 +1,2 @@
-// export const BaseBackURL = "http://31.7.74.129/";
-export const BaseBackURL = "http://localhost:5000/";
+export const BaseBackURL = "http://212.33.199.12/";
+// export const BaseBackURL = "http://localhost:5000/";
